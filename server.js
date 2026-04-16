93e3a1e049ce4ad36449909fa38a2bb2b9b6392c
egonzalezqw <egofac@gmail.com>
2026-04-16 17:29:00 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('🚀 Hola OpenShift! App desplegada correctamente desde GitHub.');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'UP', time: new Date() });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
